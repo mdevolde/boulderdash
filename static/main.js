8a0de6acd9f054300d24397f79d028e793db69a3
= <martin.devolder2@gmail.com>
2024-08-29 15:32:16 +0200

    Updating main.js to patch delay when long key press

--- static/main.js
+++ static/main.js
@@ -5,11 +5,15 @@ async function run() {
 
     let lastTime = 0;
     const tickDuration = 1000 / 10;
-
     let game = await new Game();
+    const keysPressed = {};
 
     document.addEventListener('keydown', (event) => {
-        game.key_down(event.key);
+        keysPressed[event.key] = true;
+    });
+
+    document.addEventListener('keyup', (event) => {
+        keysPressed[event.key] = false;
     });
 
     function gameLoop(timestamp) {
@@ -17,6 +21,20 @@ async function run() {
 
         if (deltaTime >= tickDuration) {
             lastTime = timestamp;
+
+            if (keysPressed['ArrowRight']) {
+                game.key_down('ArrowRight');
+            }
+            if (keysPressed['ArrowLeft']) {
+                game.key_down('ArrowLeft');
+            }
+            if (keysPressed['ArrowUp']) {
+                game.key_down('ArrowUp');
+            }
+            if (keysPressed['ArrowDown']) {
+                game.key_down('ArrowDown');
+            }
+
             game.update();
         }
 
